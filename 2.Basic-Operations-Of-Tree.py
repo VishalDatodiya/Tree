19b6c4d33583bee2dc1a4c00e102feadede7a7ad
vishalDatodiya <vishaldatodiya12@gmail.com>
2023-03-12 00:45:27 +0530

    Nodes at K distance, Nodes without siblings

--- 2.Basic-Operations-Of-Tree.py
+++ 2.Basic-Operations-Of-Tree.py
@@ -58,7 +58,62 @@ def HeightOfTree(root):
         return left + 1
     return right + 1
 
+
+def NodesAtKDistance(root,k):
+    if root == None:
+        return
+    if k == 0:
+        print(root.data,end=" ")
+        return
+    NodesAtKDistance(root.left,k-1)
+    NodesAtKDistance(root.right,k-1)
+    
+    
+    # 11                0      
+# 12      13        1       1
+
+def ReplaceNodeWithDept(root, d=0): # d is a variable with default value 0
+    if root == None:
+        return
+    root.data = d
+    ReplaceNodeWithDept(root.left,d+1)
+    ReplaceNodeWithDept(root.right,d+1)
      
+# Find Node in tree
+def isNodePresent(root, x):
+    if root == None:
+        return False
+    if root.data == x:
+        return True
+
+    left = isNodePresent(root.left,x)
+    right = isNodePresent(root.right,x)
+    return left or right  
+
+# Node without siblings
+
+def printNodesWithoutSibling(root) :
+    if root == None:
+        return -1
+    
+    if printNodesWithoutSibling(root.left) == -1 and root.right != None:
+        print(root.right.data, end=" ")
+    if printNodesWithoutSibling(root.right) and root.left != None:
+        print(root.left.data, end=" ")
+
+
+def removeLeafNodes(root):
+    if root == None:
+        return None
+    
+    if root.left == None and root.right == None:
+        return None
+    
+    root.left = removeLeafNodes(root.left)
+    root.right = removeLeafNodes(root.right)
+    
+    return root
+
 def takeInput():
     print("Root Data: ")
     root_data = int(input())
@@ -84,3 +139,14 @@ print("total sum of nodes : ", sumOfNodes(root))
 print("Max node : ", findMaxNode(root))
 print("Min node : ", findMinNode(root))
 print("Height of Tree : ", HeightOfTree(root))
+print("Nodes At k distance : ")
+NodesAtKDistance(root,k=2)
+print("replace node with k dept : ")
+ReplaceNodeWithDept(root)
+printNode(root)
+print("Node is Present : ",isNodePresent(root,5))
+print("Nodes without siblings : ")
+printNodesWithoutSibling(root)
+
+removeLeafNodes(root)
+printNode(root)
